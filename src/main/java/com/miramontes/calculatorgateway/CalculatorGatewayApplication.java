82cca1fefdd2b1bc98542d471f704da28d55b5d8
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-09-13 11:55:07 -0500

    init commit

---
+++ src/main/java/com/miramontes/calculatorgateway/CalculatorGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.miramontes.calculatorgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miramontes/calculatorgateway/CalculatorGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miramontes.calculatorgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
